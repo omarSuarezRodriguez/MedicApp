91ddacb7cdee39ed0ef90c5013031232f030189f
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-06-05 09:01:46 -0500

    Commit

--- app/src/main/java/omar/cursos/medicapp/MainActivity.java
+++ app/src/main/java/omar/cursos/medicapp/MainActivity.java
@@ -1,7 +1,11 @@
 package omar.cursos.medicapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_pacientes:
+                            selectedFragment = new PacientesFragment();
+                            break;
+                        case R.id.nav_agenda:
+                            selectedFragment = new AgendaFragment();
+                            break;
+                        case R.id.nav_mensajes:
+                            selectedFragment = new MensajesFragment();
+                            break;
+                        case R.id.nav_contenido:
+                            selectedFragment = new ContenidoFragment();
+                            break;
+
+
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+
 }
